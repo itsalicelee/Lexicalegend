aa476b6bc5ed92ea089eb66dd4273b469fc92711
itsalicelee <alice668149@gmail.com>
2022-07-21 17:42:53 +0800

    reply fetching result ts

--- src/cambridge.ts
+++ src/cambridge.ts
@@ -1,16 +1,15 @@
-const cheerio = require('cheerio')
-const axios = require("axios");
-
+import axios from 'axios';
+import Cheerio from 'cheerio';
 
 const AxiosInstance = axios.create();  // Create a new Axios Instance
 
 // Send an async HTTP Get request to the url
-async function fetchCambridge(text){
+export async function fetchCambridge(text: string): Promise<string>{
     const url = `https://dictionary.cambridge.org/us/dictionary/english-chinese-traditional/${text}`; 
     try{
         const response = await AxiosInstance.get(url);
         const html = response.data;
-        const $ = cheerio.load(html); // Load the HTML string into cheerio
+        const $ = Cheerio.load(html); // Load the HTML string into cheerio
         const statsTable = $('.def-body').text(); // Parse the HTML and extract text
         console.log(statsTable); // Log the number of captured elements
         return statsTable;
@@ -19,3 +18,5 @@ async function fetchCambridge(text){
         throw new Error('Failed fetching QQ');
     };
 }
+
+fetchCambridge("agony");
