75488ef8f7203fe425f209109191fccd861e582b
itsalicelee <alice668149@gmail.com>
2022-07-21 16:26:25 +0800

    reply fetching result

--- src/cambridge.js
+++ src/cambridge.js
@@ -1,17 +1,18 @@
-import axios from 'axios';
-import Cheerio from 'cheerio';
+const cheerio = require('cheerio')
+const axios = require("axios");
+
 
 const AxiosInstance = axios.create();  // Create a new Axios Instance
 
 // Send an async HTTP Get request to the url
-export async function fetchCambridge(text: string): Promise<string>{
+async function fetchCambridge(text){
     const url = `https://dictionary.cambridge.org/us/dictionary/english-chinese-traditional/${text}`; 
     try{
         const response = await AxiosInstance.get(url);
         const html = response.data;
-        const $ = Cheerio.load(html); // Load the HTML string into cheerio
+        const $ = cheerio.load(html); // Load the HTML string into cheerio
         const statsTable = $('.def-body').text(); // Parse the HTML and extract text
-        // console.log(statsTable); // Log the number of captured elements
+        console.log(statsTable); // Log the number of captured elements
         return statsTable;
     }
     catch(e){
