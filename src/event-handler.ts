d5e8182d4d79c35027a36f38d54e298fb65ef5fc
itsalicelee <alice668149@gmail.com>
2022-07-29 14:17:52 +0800

    fix: fix toLowerCase in suggestEventHandler

--- src/event-handler.ts
+++ src/event-handler.ts
@@ -197,7 +197,7 @@ export const suggestEventHandler = async (client: Client, text: string, replyTok
     console.log("Suggest Event Handler!");
 
     var suggestedWord: string = '';
-    text = text.toLowerCase().trim();
+    text = text.toUpperCase().trim();
     if(text === 'TOEFL'){
         suggestedWord = utils.suggestWord('toefl');
     }
